f2a38dfce43a20c37eb9f38f164c7556d8cb6681
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-01-23 15:11:47 -0300

    Controller usuario

--- ControleFinanceiro.WebApp/Controllers/UsuarioController.cs
+++ ControleFinanceiro.WebApp/Controllers/UsuarioController.cs
@@ -13,4 +13,90 @@ namespace ControleFinanceiro.WebApp.Controllers
         {
             this.service = service;
         }
+        public async Task<IActionResult> GetAllUsuario()
+        {
+            var listaUsuario = await service.GetAll();
+            return View(listaUsuario);
+        }
+        // GET: UsuarioController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Details/5
+        public async Task<IActionResult> ConsultUsuario(Guid id)
+        {
+            var consulta = await service.GetById(id);
+            return View(consulta);
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult CreateUsuario()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateUsuario(UsuarioService usuarioService)
+        {
+            try
+            {
+                await service.AddSave(usuarioService);
+                return RedirectToAction(nameof(GetAllUsuario));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public async Task<IActionResult> EditUsuario(Guid id)
+        {
+            var usuario = await service.GetById(id);
+            return View(usuario);
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUsuario(UsuarioService usuarioService)
+        {
+            try
+            {
+                var usuario = await service.Update(usuarioService);
+                return RedirectToAction(nameof(GetAllUsuario));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public async Task<IActionResult> DeleteUsuario(Guid id)
+        {
+            var usuarioDelete = await service.GetById(id);
+            return PartialView("DeleteUsuario", usuarioDelete);
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUsuario(UsuarioService usuario)
+        {
+            try
+            {
+                await service.MarkDeleted(usuario);
+                return RedirectToAction(nameof(GetAllUsuario));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
+}
