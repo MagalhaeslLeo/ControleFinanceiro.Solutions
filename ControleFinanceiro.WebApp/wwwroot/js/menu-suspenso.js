3dc7e8648e7e57ecfdae478698e11fd311b4d35e
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-02-20 20:46:23 -0300

    Modificações javascript, obter módulos menu suspenso

--- ControleFinanceiro.WebApp/wwwroot/js/menu-suspenso.js
+++ ControleFinanceiro.WebApp/wwwroot/js/menu-suspenso.js
@@ -1,24 +1,27 @@
 ﻿$(document).ready(function () {
-
     $('#menu-suspenso').click(function () {
-        loadMenuSuspenso();
-        $('#spinner-div').show();
+        document.querySelector('.menu-nav').style.display = 'block';
+       
     });
+    //$('#menu-suspenso').click(function () {
+    //    loadMenuSuspenso();
+    //    $('#spinner-div').show();
+    //});
 
-    function loadMenuSuspenso() {
-        $.ajax({
-            url: '/ModuloMenu/GetAllModuloMenuSuspenso',
-            type: 'POST',
-            success: function (response) {
-                $('#navigator ul').html(response);
-                document.querySelector('.menu-nav').style.display = 'block';
-            },
-            complete: function () {
-                $('#spinner-div').hide();
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    }
+    //function loadMenuSuspenso() {
+    //    $.ajax({
+    //        url: '/ModuloMenu/GetAllModuloMenuSuspenso',
+    //        type: 'POST',
+    //        success: function (response) {
+    //            $('#navigator ul').html(response);
+    //            document.querySelector('.menu-nav').style.display = 'block';
+    //        },
+    //        complete: function () {
+    //            $('#spinner-div').hide();
+    //        },
+    //        error: function (error) {
+    //            console.log(error);
+    //        }
+    //    });
+    //}
 });
