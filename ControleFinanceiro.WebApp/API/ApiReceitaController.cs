a17a0d03fedab66e26ceed581a02afbd6c23c24d
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-03-19 06:42:24 -0300

    Ajustando a API Receita

--- ControleFinanceiro.WebApp/API/ApiReceitaController.cs
+++ ControleFinanceiro.WebApp/API/ApiReceitaController.cs
@@ -1,4 +1,5 @@
 ﻿using ControleFinanceiro.Service.Interfaces;
+using ControleFinanceiro.Service.ServiceEntity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleFinanceiro.WebApp.API
@@ -18,6 +19,44 @@ namespace ControleFinanceiro.WebApp.API
         {
             var listaReceita = await serviceReceita.GetAll();
             return Ok(listaReceita);
+        }
+        [HttpPost]
+        [Route("AdicionarReceita")]
+        public async Task<IActionResult> AdicionarReceita([FromBody] ReceitaService receitaService)
+        {
+            try
+            {
+                await serviceReceita.AddSave(receitaService);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception(ex.Message, ex));
+            }
+        }
+        [HttpPost("DeleteReceita")]
+        [Route("DeleteReceita/{id:Guid}")]
+        public async Task<IActionResult> DeleteReceita([FromRoute] Guid Id)
+        {
+            try
+            {
+                if (Id != Guid.Empty)
+                {
+                    await serviceReceita.MarkDeleted(Id);
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception(ex.Message, ex));
+            }
+        }
+        [HttpGet]
+        [Route("GetByIdReceita")]
+        public async Task<IActionResult> GetByIdReceita(Guid Id)
+        {
+            var receita = await serviceReceita.GetById(Id);
+            return Ok(receita);
         }
     }
 }
--- ControleFinanceiro.WebApp/Controllers/ReceitaController.cs
+++ ControleFinanceiro.WebApp/Controllers/ReceitaController.cs
@@ -29,78 +29,25 @@ namespace ControleFinanceiro.WebApp.Controllers
         }
 
         // GET: ReceitaController/Details/5
-        public async Task<IActionResult> ConsultReceita(Guid id)
+        public IActionResult ConsultReceita(Guid id)
         {
-            var consulta = await service.GetById(id);
-            return PartialView(consulta);
+           ViewBag.Id = id;
+           ViewBag.Action = "Consult";
+           return View("CreateReceita");
         }
 
         // GET: ReceitaController/Create
         public ActionResult CreateReceita()
         {
-            return View();
-        }
-
-        // POST: ReceitaController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateReceita(ReceitaService receitaService)
-        {
-            try
-            {
-                await service.AddSave(receitaService);
-                return RedirectToAction(nameof(GetAllReceita));
-            }
-            catch
-            {
-                return View();
-            }
+            return View("CreateReceita");
         }
 
         // GET: ReceitaController/Edit/5
-        public async Task<IActionResult> EditReceita(Guid id)
+        public IActionResult EditReceita(Guid id)
         {
-            var receita = await service.GetById(id);
-            return PartialView(receita);
-        }
-
-        // POST: ReceitaController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditReceita(ReceitaService receitaService)
-        {
-            try
-            {
-                var receita = await service.Update(receitaService);
-                return RedirectToAction(nameof(GetAllReceita));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReceitaController/Delete/5
-        public async Task<IActionResult> DeleteReceita(Guid id)
-        {
-            var receitaDelete = await service.GetById(id);
-            return PartialView("DeleteReceita", receitaDelete);
-        }
-
-        // POST: ReceitaController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task <IActionResult> DeleteReceita(ReceitaService receita)
-        {
-            try
-            {
-                await service.MarkDeleted(receita);
-                return RedirectToAction(nameof(GetAllReceita));
-            }
-            catch
-            {
-                return View();
-            }
+            ViewBag.Id = id;
+            ViewBag.Action = "Edit";
+            return View("CreateReceita");
         }
     }
 }
