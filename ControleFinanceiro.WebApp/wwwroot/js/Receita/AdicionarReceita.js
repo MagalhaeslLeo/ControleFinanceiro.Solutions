b78a079536a78dd6735274ba01ae33c72b81933c
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-03-23 13:36:33 -0300

    Iniciando implementação módulo cadastro receita

---
+++ ControleFinanceiro.WebApp/wwwroot/js/Receita/AdicionarReceita.js
@@ -0,0 +1,132 @@
+﻿var KTInputmask = function () {
+
+    // Private functions
+    var frmMask = function () {
+        //CNPJ
+        $("#cnpj").inputmask("mask", {
+            "mask": "99.999.999/9999-99"
+        });
+
+        //Telefone
+        $("#telefone").inputmask("mask", {
+            "mask": "(99) 9999-9999"
+        });
+
+        //Cep
+        $("#cep").inputmask("mask", {
+            "mask": "99999-999"
+        });
+
+        //Email
+        $("#email").inputmask({
+            mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+            greedy: false,
+            onBeforePaste: function (pastedValue, opts) {
+                pastedValue = pastedValue.toLowerCase();
+                return pastedValue.replace("mailto:", "");
+            },
+            definitions: {
+                '*': {
+                    validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+                    cardinality: 1,
+                    casing: "lower"
+                }
+            }
+        });
+    }
+
+    return {
+        // public functions
+        init: function () {
+            frmMask();
+        }
+    };
+}();
+
+jQuery(document).ready(function () {
+    KTInputmask.init();
+});
+
+var frmSave = (function () {
+
+    $('#btnSalvar').on('click', function () {
+
+        var periodoDate = $('[name="periodo"]').val();
+        // Criando novo objeto do tipo Date com o valor vindo do formulário e colocado na variável periodoValue
+
+        var periodoValue = new Date(periodoDate);
+
+        // Convertendo o valor do objeto do tipo Date para um objeto do tipo DateTime
+        var periodoFormatado = periodoValue ? periodoValue.toISOString() : null;
+        var post = {};
+
+        post = {
+            id: $('[name="Id"]').val || null,
+            descricao: $('[name="descricao"]').val() || null,
+            periodo: periodoFormatado,
+            valor: $('[name="valor"]').val() || null
+        };
+
+        var json = JSON.stringify(post);
+
+        $.ajax({
+            method: 'POST',
+            url: '/api/ApiDespesa/AdicionarDespesa',
+            data: json,
+            contentType: 'application/json',
+        }).done(function (response) {
+
+            if (response === true) {
+                Swal.fire({
+                    title: "Negado !",
+                    text: "Despesa já Cadastrada",
+                    icon: "error",
+                    buttonsStyling: false,
+                    confirmButtonText: "Ok!",
+                    customClass: {
+                        confirmButton: "btn btn-success"
+                    }
+                }).then(function () {
+                    window.location.href = "/Despesa/GetAllDespesa";
+                });
+            } else {
+                Swal.fire({
+                    title: "Concluido!",
+                    text: "Com Sucessso",
+                    icon: "success",
+                    buttonsStyling: false,
+                    confirmButtonText: "Ok!",
+                    customClass: {
+                        confirmButton: "btn btn-success"
+                    }
+                }).then(function () {
+                    window.location.href = "/Despesa/GetAllDespesa";
+                });
+            }
+
+        });
+    });
+
+    if ($('#Id').val() !== '') {
+        $.ajax({
+            method: 'Get',
+            url: '/api/ApiDespesa/GetByIdDespesa?Id=' + $('#Id').val(),
+        }).done(function (response) {
+
+            $('#descricao').val(response.descricao);
+
+            var periodo = new Date(response.periodo);
+            var periodoFormatado = periodo.toLocaleDateString('pt-BR');
+
+            $('#periodo').val(periodoFormatado);
+            $('#valor').val(response.valor);
+
+            if ($('#action').val() === 'Consult') {
+                $('.readOnly').attr('readonly', 'readonly');
+                $('.readOnly').prop("disabled", true);
+                $('.btnHide').hide();
+                $('a.voltar').text('Voltar');
+            }
+        });
+    }
+})(); 
---
+++ ControleFinanceiro.WebApp/wwwroot/js/Receita/ListaReceita.js
@@ -0,0 +1,149 @@
+﻿"use strict";
+// Class definition
+
+var DataContractStatus = function () {
+    // Private functions
+
+    // basic demo
+    var buildTable = function () {
+
+        var datatable = $('#kt_datatable').KTDatatable({
+            // datasource definition
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        url: '/api/ApiDespesa/GetDespesa/'
+                    }
+                },
+                pageSize: 10,
+                serverPaging: true,
+                serverFiltering: true,
+                serverSorting: true,
+            },
+
+            // layout definition
+            layout: {
+                scroll: false,
+                footer: false,
+            },
+
+            // column sorting
+            sortable: true,
+
+            pagination: true,
+
+            search: {
+                input: $('#kt_datatable_search_query'),
+                key: 'generalSearch'
+            },
+
+            // columns definition
+            columns: [
+                {
+
+                    field: 'descricao',
+                    title: 'Descrição',
+                    sortable: 'asc',
+                    type: 'text',
+                    selector: false,
+                    textAlign: 'left',
+                },
+
+                {
+
+                    field: 'periodo',
+                    title: 'Data da despesa',
+                    sortable: 'asc',
+                    type: 'text',
+                    selector: false,
+                    textAlign: 'left',
+                },
+
+
+                {
+                    field: 'valor',
+                    title: 'Valor',
+                    sortable: 'asc',
+                    type: 'text',
+                    selector: false,
+                    textAlign: 'left',
+                },
+                {
+                    field: 'Actions',
+                    title: 'Ações',
+                    sortable: false,
+                    width: 200,
+                    overflow: 'visible',
+                    autoHide: false,
+                    template: function (item) {
+                        return '\
+                            <a href="/Despesa/ConsultDespesa/'+ item.id + '" class="btn btn-sm btn-clean btn-icon mr-2">\
+                                <i class="far fa-eye"></i>\
+                            </a>\
+                            <a href="/Despesa/EditDespesa/'+ item.id + '" class="btn btn-sm btn-clean btn-icon mr-2 update">\
+                                <i class="fas fa-pencil-alt"></i>\
+                            </a>\
+                             <a data-id="' + item.id + '" class="btn btn-sm btn-clean btn-icon mr-2 btn-deletar-item delete">\
+                             <i class="fas fa-trash-alt"></i>\
+                            </a>';
+                    },
+
+
+                }
+            ],
+
+        });
+        $('#kt_datatable').on('click', '.delete', function () {
+
+            var delet = $(this).data('id');
+
+            Swal.fire({
+                title: "Excluir ?",
+                text: "Não Será possível reverte esta exclusão !",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Ok",
+                cancelButtonText: " Cancelar",
+                reverseButtons: true
+            }).then(function (result) {
+                if (result.value) {
+
+                    $.ajax({
+                        method: 'POST',
+                        url: '/Api/ApiDespesa/DeleteDespesa/' + delet,
+                    }).done(function (response) {
+
+                        Swal.fire({
+                            title: "Concluido!",
+                            text: "Dados Deletados !",
+                            icon: "success",
+                            buttonsStyling: false,
+                            confirmButtonText: "Ok!",
+                            customClass: {
+                                confirmButton: "btn btn-success"
+                            }
+                        }).then(function () {
+                            $('#kt_datatable').KTDatatable('reload');
+                        });
+
+                    });
+                }
+
+            });
+
+
+        });
+    };
+
+    return {
+        // public functions
+        init: function () {
+            buildTable();
+        },
+    };
+}()
+
+jQuery(document).ready(function () {
+    DataContractStatus.init();
+});
